d99705858e10bb291f4905cb5174eeea497a7af0
Christopher <jcp.christopher@gmail.com>
2018-10-28 23:03:46 +0530

    generate events in random

--- lib/mars_web/controllers/event_controller.ex
+++ lib/mars_web/controllers/event_controller.ex
@@ -16,23 +16,42 @@ defmodule MarsWeb.EventController do
   """
 
   @doc """
-  A temporary metbod to create a random event and enqueue it to EventCollector Genstage
+  A temporary metbod to generate random events and enqueue it to EventCollector Genstage
 
   Returns HTTP Status code 200
   """
   def create_event(conn, _) do
     Logger.debug "creating event"
 
-    event = %Event {
-      app_id: "1",
-      message_id: "100",
-      event: "MESSAGE_CREATED",
-      created_at: Timex.now
+    event_map = %{
+      1 => "MESSAGE_CREATED",
+      2 => "MESSAGE_REACHED_BACKEND",
+      3 => "MESSAGE_IN_SEARCH_QUEUE",
+      4 => "MESSAGE_IN_UPDATES_QUEUE",
+      5 => "MESSAGE_IN_WS_QUEUE",
+      6 => "MESSAGE_REACHED_FRONTEND"
     }
-    
-    EventCollector.enqueue(event)
 
-    Logger.debug "Event queued"
+    event_map_len = event_map |> map_size
+
+    events = for i <- 0..10 do
+      
+      random_num = :rand.uniform(event_map_len)
+
+      random_event = Map.get(event_map, random_num)
+      
+      event = %Event {
+        app_id: i,
+        message_id: i * 100,
+        event: random_event,
+        created_at: Timex.now
+      }
+
+      Logger.debug "Random event #{inspect event}"
+
+      EventCollector.enqueue(event)
+      Logger.debug "Event queued"
+    end
 
     conn
     |> put_status(:ok)
