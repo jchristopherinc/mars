518ffd8ac980d7c451c289edba0d30494f1abc4d
Christopher <jcp.christopher@gmail.com>
2018-11-24 19:01:01 +0530

    genstage intervals

--- lib/mars_web/controllers/event_controller.ex
+++ lib/mars_web/controllers/event_controller.ex
@@ -101,7 +101,7 @@ defmodule MarsWeb.EventController do
 
       random_event = Map.get(event_map, random_num)
 
-      random_id = :rand.uniform(10_000)
+      random_id = :rand.uniform(100_000)
 
       event = %{
         app_id: random_id,
