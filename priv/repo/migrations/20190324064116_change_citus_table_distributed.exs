3bd082aed174fdc0f58fb3550ccd1d10ccb43eb1
Christopher <jcp.christopher@gmail.com>
2019-03-25 11:17:44 +0530

    working citus version :pig:

--- priv/repo/migrations/20190324064116_change_citus_table_distributed.exs
+++ priv/repo/migrations/20190324064116_change_citus_table_distributed.exs
@@ -3,7 +3,6 @@ defmodule Mars.Repo.Migrations.ChangeCitusTableDistributed do
 
   def change do
     execute "SET citus.shard_replication_factor = 1;"
-    drop unique_index(:event, [:app_id, :message_id])
     execute "CREATE EXTENSION IF NOT EXISTS citus"
     execute "select create_distributed_table('event', 'message_id')"
   end
