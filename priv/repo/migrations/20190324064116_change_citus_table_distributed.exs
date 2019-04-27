a109958cb549ede8d983c3af8183d52528a5eaea
Christopher <jcp.christopher@gmail.com>
2019-04-27 10:02:09 +0530

    comment out citus part

--- priv/repo/migrations/20190324064116_change_citus_table_distributed.exs
+++ priv/repo/migrations/20190324064116_change_citus_table_distributed.exs
@@ -2,8 +2,8 @@ defmodule Mars.Repo.Migrations.ChangeCitusTableDistributed do
   use Ecto.Migration
 
   def change do
-    execute "SET citus.shard_replication_factor = 1;"
-    execute "CREATE EXTENSION IF NOT EXISTS citus"
-    execute "select create_distributed_table('event', 'message_id')"
+    # execute "SET citus.shard_replication_factor = 1;"
+    # execute "CREATE EXTENSION IF NOT EXISTS citus"
+    # execute "select create_distributed_table('event', 'message_id')"
   end
 end
